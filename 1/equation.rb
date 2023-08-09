abf27c18f67260aa866333484ce6420bf7702041
egoruska85 <egorarbuzov@gmail.com.com>
2023-08-09 15:43:59 +0300

    Lesson 1(refactoring)

--- 1/equation.rb
+++ 1/equation.rb
@@ -15,8 +15,8 @@ puts "D = #{ d = b.to_i ** 2 - 4 * a.to_i * c.to_i }"
 if d > 0
   puts "Дискриминант больше 0 и имеет два корня"
   puts "Решение"
-  puts "#{ (b.to_i + (d**(1.0/2.0))) / (2 * a.to_i) } - X1"
-  puts "#{ (b.to_i - (d**(1.0/2.0))) / (2 * a.to_i) } - X2"
+  puts "#{ (b.to_i + Math.sqrt(d)) / (2 * a.to_i) } - X1"
+  puts "#{ (b.to_i - Math.sqrt(d)) / (2 * a.to_i) } - X2"
 elsif d == 0
   puts "Дискриминант равен 0 и имеет один корень"
   puts "Решение"
