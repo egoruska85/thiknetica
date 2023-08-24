3d17b53424bae8cb43eb02eb698229600e45dcc5
egoruska85 <egorarbuzov@gmail.com.com>
2023-08-24 13:13:55 +0300

    Lesson 3 finish !

--- 3/test/station.rb
+++ 3/test/station.rb
@@ -1,24 +1,20 @@
 class Station
   attr_reader :trains, :name
-  ### Созлание станции #######
-  attr_reader :name, :trains, :sent_trains, :arrived
+  
   def initialize(name)
     @name = name
     @trains = []
-    @sent_trains = []
-    @arrived = []
   end
+
   def add_train(train)
-    @arrived << train.number
     @trains << train
   end
+
   def send_train(train)
-    @sent_trains << train.number
     @trains.delete(train)
   end
-  ######### Вывод всех поездов находящие на стации #####
-  def show_trains(type_train)
-    type_trains = { 1 => 'Пассажирский', 2 => 'Грузовой' }
-    trains.select{ |train| train.type_train == type_trains[type_train] }
+
+  def show_trains(type)
+    trains.select{ |train| train.type == type }
   end
 end
--- 3/test/train.rb
+++ 3/test/train.rb
@@ -1,23 +1,24 @@
 class Train
-  attr_reader :type_train, :speed, :route, :carriages_amount, :number
-  ##### Создание поезда ######
-  def initialize(number, type_train, quantity)
+  attr_reader :type, :speed, :route, :carriages_amount, :number
+  def initialize(number, type, carriages_amount)
     @number = number
-    type_trains = { 1 => 'Пассажирский', 2 => 'Грузовой' }
-    @type_train = type_trains[type_train]
-    @carriages_amount = quantity
+    @type = type
+    @carriages_amount = carriages_amount
     @speed = 0
   end
+
   def next_station
-    route.stations[@num_station + 1]
+    route.stations[@number_station + 1] if route.stations[@number_station] != route.stations.last
   end
+
   def previous_station
-    route.stations[@num_station - 1] unless @num_station == 0
+    route.stations[@number_station - 1] unless @num_station == 0
   end
+
   def current_station
-    route.stations[@num_station]
+    route.stations[@number_station]
   end
-  ###### Ручное управление ######
+
   def start(i)
     @speed = i
     puts @speed
@@ -28,51 +29,58 @@ class Train
     @speed += i
     puts @speed
   end
+
   def down_speed(i)
     @speed -= i if @speed > 0
     puts @speed
   end
+
   def stop
     @speed = 0
   end
+
   def start_continue(i)
     @speed = i
   end
+
   def stop_station
+    next_station.add_train(self) if
+    @number_station += 1
     @speed = 0
-    next_station.add_train(self)
-    @num_station += 1
   end
+
   def up_to_carriages(quantity)
     @carriages_amount += quantity if @speed == 0
   end
+
   def down_to_carriages(quantity)
     @carriages_amount -= quantity if @speed == 0 && carriages_amount > quantity
   end
+
   def show_carriages
     puts carriages_amount
   end
 
-  #####Указание маршрута#####
   def enter_route(route)
     @route = route
-    @num_station = 0
+    @number_station = 0
     route.start_station.add_train(self)
   end
-  #### Передвижение поезда между стациями #####
+
   def move_to_next_station
-    if next_station
-      current_station.send_train(self)
-      next_station.add_train(self)
-      @num_station+= 1
-    end
+    return unless next_station
+
+    current_station.send_train(self)
+    next_station.add_train(self)
+    @number_station += 1
   end
+
   def move_to_previous_station
-    if previous_station
-      current_station.send_train(self)
-      previous_station.add_train(self)
-      @num_station -= 1
-    end
+    return unless previous_station
+
+    current_station.send_train(self)
+    previous_station.add_train(self)
+    @number_station -= 1
   end
 
 end
